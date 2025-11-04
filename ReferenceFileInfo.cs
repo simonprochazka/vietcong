2918f6acfc64cd7e9f302f9c3f8595ddf5ae6143
simonprochazka <simon.prochazka.alchemica@gmail.com>
2025-11-04 14:23:13 +0100

    Add files via upload

---
+++ ReferenceFileInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+// AUTOMATICKY VYGENEROVANÝ KÓD - NEUPRAVOVAT RUČNĚ!
+// Vygenerováno: 2025-09-08 16:16:01
+// Zdrojová složka: C:\Users\simon\Desktop\Vietcong - Gold Edition
+// Počet .cbf souborů: 35
+// Kontroluje se pouze velikost souborů
+
+public class FileMetadata
+{
+    public long Size { get; set; }
+}
+
+class ReferenceFileInfo
+{
+    public static readonly Dictionary<string, FileMetadata> FileInfos = new Dictionary<string, FileMetadata>
+    {
+        [@"setup.cbf"] = new FileMetadata { Size = 469987L },
+        [@"vietcong_101.cbf"] = new FileMetadata { Size = 7161330L },
+        [@"vietcong_120.cbf"] = new FileMetadata { Size = 19557227L },
+        [@"vietcong_130.cbf"] = new FileMetadata { Size = 30123578L },
+        [@"vietcong_140.cbf"] = new FileMetadata { Size = 4405470L },
+        [@"vietcong_150.cbf"] = new FileMetadata { Size = 723479L },
+        [@"vietcong_160.cbf"] = new FileMetadata { Size = 43057848L },
+        [@"vietcong_cz_01.cbf"] = new FileMetadata { Size = 702189625L },
+        [@"vietcong_cz_02.cbf"] = new FileMetadata { Size = 510734596L },
+        [@"vietcong_cz_dub.cbf"] = new FileMetadata { Size = 109497496L },
+        [@"vietcong_cz_gsp.cbf"] = new FileMetadata { Size = 48713704L },
+        [@"vietcong_cz_xtl.cbf"] = new FileMetadata { Size = 1995521L },
+        [@"addons\\fistalpha\\fistalpha_01.cbf"] = new FileMetadata { Size = 661309082L },
+        [@"addons\\fistalpha\\fistalpha_02.cbf"] = new FileMetadata { Size = 79552875L },
+        [@"addons\\fistalpha\\fistalpha_03.cbf"] = new FileMetadata { Size = 385790L },
+        [@"addons\\fistalpha\\fistalpha_cze_dub.cbf"] = new FileMetadata { Size = 42630781L },
+        [@"addons\\fistalpha\\fistalpha_cze_gen.cbf"] = new FileMetadata { Size = 19469113L },
+        [@"addons\\fistalpha\\fistalpha_cze_snd.cbf"] = new FileMetadata { Size = 1522138L },
+        [@"addons\\fistalpha\\fistalpha_cze_xtl.cbf"] = new FileMetadata { Size = 498425L },
+        [@"hradba\\versions\\hradba00112.cbf"] = new FileMetadata { Size = 71114L },
+        [@"hradba\\versions\\hradba00200.cbf"] = new FileMetadata { Size = 71212L },
+        [@"hradba\\versions\\hradba00202.cbf"] = new FileMetadata { Size = 91230L },
+        [@"maps\\3SF-BRIDGE.cbf"] = new FileMetadata { Size = 89149L },
+        [@"maps\\BeerParty.cbf"] = new FileMetadata { Size = 988L },
+        [@"maps\\Cement_Dust.cbf"] = new FileMetadata { Size = 69451L },
+        [@"maps\\Depot.cbf"] = new FileMetadata { Size = 76444L },
+        [@"maps\\DustGlas.cbf"] = new FileMetadata { Size = 97969L },
+        [@"maps\\DustWorld.cbf"] = new FileMetadata { Size = 69281L },
+        [@"maps\\Frontier.cbf"] = new FileMetadata { Size = 155140L },
+        [@"maps\\Halong-V1.9.cbf"] = new FileMetadata { Size = 720973L },
+        [@"maps\\indiancountry.cbf"] = new FileMetadata { Size = 14782646L },
+        [@"maps\\Midi.cbf"] = new FileMetadata { Size = 97890L },
+        [@"maps\\Osgiliath-City.cbf"] = new FileMetadata { Size = 73544L },
+        [@"maps\\RiverDale.cbf"] = new FileMetadata { Size = 162351L },
+        [@"maps\\threecanyons.cbf"] = new FileMetadata { Size = 7968524L },
+    };
+}
