6b4b577be3c38c16038dff4e53ed80f0da41e698
simonprochazka <simon.prochazka.alchemica@gmail.com>
2026-01-14 17:07:22 +0100

    Add files via upload

--- ReferenceFileInfo.cs
+++ ReferenceFileInfo.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 
 // AUTOMATICKY VYGENEROVANÝ KÓD - NEUPRAVOVAT RUČNĚ!
-// Vygenerováno: 2025-09-08 16:16:01
-// Zdrojová složka: C:\Users\simon\Desktop\Vietcong - Gold Edition
-// Počet .cbf souborů: 35
+// Vygenerováno: 2026-01-14 16:59:41
+// Zdrojová složka: C:\Users\simon\Desktop\VietcongGame
+// Počet souborů (.cbf + .dll): 45
 // Kontroluje se pouze velikost souborů
 
 public class FileMetadata
@@ -16,40 +16,50 @@ class ReferenceFileInfo
 {
     public static readonly Dictionary<string, FileMetadata> FileInfos = new Dictionary<string, FileMetadata>
     {
-        [@"setup.cbf"] = new FileMetadata { Size = 469987L },
-        [@"vietcong_101.cbf"] = new FileMetadata { Size = 7161330L },
-        [@"vietcong_120.cbf"] = new FileMetadata { Size = 19557227L },
-        [@"vietcong_130.cbf"] = new FileMetadata { Size = 30123578L },
-        [@"vietcong_140.cbf"] = new FileMetadata { Size = 4405470L },
-        [@"vietcong_150.cbf"] = new FileMetadata { Size = 723479L },
-        [@"vietcong_160.cbf"] = new FileMetadata { Size = 43057848L },
-        [@"vietcong_cz_01.cbf"] = new FileMetadata { Size = 702189625L },
-        [@"vietcong_cz_02.cbf"] = new FileMetadata { Size = 510734596L },
-        [@"vietcong_cz_dub.cbf"] = new FileMetadata { Size = 109497496L },
-        [@"vietcong_cz_gsp.cbf"] = new FileMetadata { Size = 48713704L },
-        [@"vietcong_cz_xtl.cbf"] = new FileMetadata { Size = 1995521L },
-        [@"addons\\fistalpha\\fistalpha_01.cbf"] = new FileMetadata { Size = 661309082L },
-        [@"addons\\fistalpha\\fistalpha_02.cbf"] = new FileMetadata { Size = 79552875L },
-        [@"addons\\fistalpha\\fistalpha_03.cbf"] = new FileMetadata { Size = 385790L },
-        [@"addons\\fistalpha\\fistalpha_cze_dub.cbf"] = new FileMetadata { Size = 42630781L },
-        [@"addons\\fistalpha\\fistalpha_cze_gen.cbf"] = new FileMetadata { Size = 19469113L },
-        [@"addons\\fistalpha\\fistalpha_cze_snd.cbf"] = new FileMetadata { Size = 1522138L },
-        [@"addons\\fistalpha\\fistalpha_cze_xtl.cbf"] = new FileMetadata { Size = 498425L },
-        [@"hradba\\versions\\hradba00112.cbf"] = new FileMetadata { Size = 71114L },
-        [@"hradba\\versions\\hradba00200.cbf"] = new FileMetadata { Size = 71212L },
-        [@"hradba\\versions\\hradba00202.cbf"] = new FileMetadata { Size = 91230L },
-        [@"maps\\3SF-BRIDGE.cbf"] = new FileMetadata { Size = 89149L },
-        [@"maps\\BeerParty.cbf"] = new FileMetadata { Size = 988L },
-        [@"maps\\Cement_Dust.cbf"] = new FileMetadata { Size = 69451L },
-        [@"maps\\Depot.cbf"] = new FileMetadata { Size = 76444L },
-        [@"maps\\DustGlas.cbf"] = new FileMetadata { Size = 97969L },
-        [@"maps\\DustWorld.cbf"] = new FileMetadata { Size = 69281L },
-        [@"maps\\Frontier.cbf"] = new FileMetadata { Size = 155140L },
-        [@"maps\\Halong-V1.9.cbf"] = new FileMetadata { Size = 720973L },
-        [@"maps\\indiancountry.cbf"] = new FileMetadata { Size = 14782646L },
-        [@"maps\\Midi.cbf"] = new FileMetadata { Size = 97890L },
-        [@"maps\\Osgiliath-City.cbf"] = new FileMetadata { Size = 73544L },
-        [@"maps\\RiverDale.cbf"] = new FileMetadata { Size = 162351L },
-        [@"maps\\threecanyons.cbf"] = new FileMetadata { Size = 7968524L },
+        [@"Vietcong - Gold Edition\\addons\\fistalpha\\fistalpha_01.cbf"] = new FileMetadata { Size = 661309082L },
+        [@"Vietcong - Gold Edition\\addons\\fistalpha\\fistalpha_02.cbf"] = new FileMetadata { Size = 79552875L },
+        [@"Vietcong - Gold Edition\\addons\\fistalpha\\fistalpha_03.cbf"] = new FileMetadata { Size = 385790L },
+        [@"Vietcong - Gold Edition\\addons\\fistalpha\\fistalpha_cze_dub.cbf"] = new FileMetadata { Size = 42630781L },
+        [@"Vietcong - Gold Edition\\addons\\fistalpha\\fistalpha_cze_gen.cbf"] = new FileMetadata { Size = 19469113L },
+        [@"Vietcong - Gold Edition\\addons\\fistalpha\\fistalpha_cze_snd.cbf"] = new FileMetadata { Size = 1522138L },
+        [@"Vietcong - Gold Edition\\addons\\fistalpha\\fistalpha_cze_xtl.cbf"] = new FileMetadata { Size = 498425L },
+        [@"Vietcong - Gold Edition\\game.dll"] = new FileMetadata { Size = 2400256L },
+        [@"Vietcong - Gold Edition\\hradba\\actual\\HRADBA.DLL"] = new FileMetadata { Size = 122880L },
+        [@"Vietcong - Gold Edition\\hradba\\versions\\hradba00112.cbf"] = new FileMetadata { Size = 71114L },
+        [@"Vietcong - Gold Edition\\hradba\\versions\\hradba00200.cbf"] = new FileMetadata { Size = 71212L },
+        [@"Vietcong - Gold Edition\\hradba\\versions\\hradba00202.cbf"] = new FileMetadata { Size = 91230L },
+        [@"Vietcong - Gold Edition\\logs.dll"] = new FileMetadata { Size = 1867776L },
+        [@"Vietcong - Gold Edition\\maps\\3SF-BRIDGE.cbf"] = new FileMetadata { Size = 89149L },
+        [@"Vietcong - Gold Edition\\maps\\BeerParty.cbf"] = new FileMetadata { Size = 988L },
+        [@"Vietcong - Gold Edition\\maps\\Cement_Dust.cbf"] = new FileMetadata { Size = 69451L },
+        [@"Vietcong - Gold Edition\\maps\\Depot.cbf"] = new FileMetadata { Size = 76444L },
+        [@"Vietcong - Gold Edition\\maps\\DustGlas.cbf"] = new FileMetadata { Size = 97969L },
+        [@"Vietcong - Gold Edition\\maps\\DustWorld.cbf"] = new FileMetadata { Size = 69281L },
+        [@"Vietcong - Gold Edition\\maps\\Frontier.cbf"] = new FileMetadata { Size = 155140L },
+        [@"Vietcong - Gold Edition\\maps\\Halong-V1.9.cbf"] = new FileMetadata { Size = 720973L },
+        [@"Vietcong - Gold Edition\\maps\\indiancountry.cbf"] = new FileMetadata { Size = 14782646L },
+        [@"Vietcong - Gold Edition\\maps\\Midi.cbf"] = new FileMetadata { Size = 97890L },
+        [@"Vietcong - Gold Edition\\maps\\Osgiliath-City.cbf"] = new FileMetadata { Size = 73544L },
+        [@"Vietcong - Gold Edition\\maps\\RiverDale.cbf"] = new FileMetadata { Size = 162351L },
+        [@"Vietcong - Gold Edition\\maps\\threecanyons.cbf"] = new FileMetadata { Size = 7968524L },
+        [@"Vietcong - Gold Edition\\msvcp120.dll"] = new FileMetadata { Size = 455328L },
+        [@"Vietcong - Gold Edition\\msvcr120.dll"] = new FileMetadata { Size = 970912L },
+        [@"Vietcong - Gold Edition\\ogg.dll"] = new FileMetadata { Size = 49152L },
+        [@"Vietcong - Gold Edition\\pd3d8.dll"] = new FileMetadata { Size = 262144L },
+        [@"Vietcong - Gold Edition\\pdcs.dll"] = new FileMetadata { Size = 65536L },
+        [@"Vietcong - Gold Edition\\setup.cbf"] = new FileMetadata { Size = 469987L },
+        [@"Vietcong - Gold Edition\\VCStarter.dll"] = new FileMetadata { Size = 65536L },
+        [@"Vietcong - Gold Edition\\vietcong_101.cbf"] = new FileMetadata { Size = 7161330L },
+        [@"Vietcong - Gold Edition\\vietcong_120.cbf"] = new FileMetadata { Size = 19557227L },
+        [@"Vietcong - Gold Edition\\vietcong_130.cbf"] = new FileMetadata { Size = 30123578L },
+        [@"Vietcong - Gold Edition\\vietcong_140.cbf"] = new FileMetadata { Size = 4405470L },
+        [@"Vietcong - Gold Edition\\vietcong_150.cbf"] = new FileMetadata { Size = 723479L },
+        [@"Vietcong - Gold Edition\\vietcong_160.cbf"] = new FileMetadata { Size = 43057848L },
+        [@"Vietcong - Gold Edition\\vietcong_cz_01.cbf"] = new FileMetadata { Size = 702189625L },
+        [@"Vietcong - Gold Edition\\vietcong_cz_02.cbf"] = new FileMetadata { Size = 510734596L },
+        [@"Vietcong - Gold Edition\\vietcong_cz_dub.cbf"] = new FileMetadata { Size = 109497496L },
+        [@"Vietcong - Gold Edition\\vietcong_cz_gsp.cbf"] = new FileMetadata { Size = 48713704L },
+        [@"Vietcong - Gold Edition\\vietcong_cz_xtl.cbf"] = new FileMetadata { Size = 1995521L },
+        [@"Vietcong - Gold Edition\\vorbis.dll"] = new FileMetadata { Size = 974848L },
     };
 }
