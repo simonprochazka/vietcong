8c314fa552416032da9f07fa04c90373d9b7ffe9
simonprochazka <simon.prochazka.alchemica@gmail.com>
2026-01-18 14:47:09 +0100

    Auto-update WhiteList.cs

--- WhiteList.cs
+++ WhiteList.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 
+// AUTOMATICKY VYGENEROVANÝ KÓD - NEUPRAVOVAT RUČNĚ!
 // WhiteList - povolené custom soubory (skiny, mapy, módy)
-// Vygenerováno: 2026-01-16
+// Vygenerováno: 2026-01-18 14:47:08
+// Počet souborů: 1
 
 public class FileMetadata
 {
@@ -13,6 +15,6 @@ public class WhiteList
 {
     public static readonly Dictionary<string, FileMetadata> AllowedFiles = new Dictionary<string, FileMetadata>
     {
-        
+        [@"testwhitelist.cbf"] = new FileMetadata { Size = 723479L },
     };
 }
