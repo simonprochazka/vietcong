cd96afb487710fbbb7a12a165ccf3612c710072b
simonprochazka <simon.prochazka.alchemica@gmail.com>
2026-01-18 14:27:37 +0100

    Create WhiteList.cs

---
+++ WhiteList.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+// WhiteList - povolené custom soubory (skiny, mapy, módy)
+// Vygenerováno: 2026-01-16
+
+public class FileMetadata
+{
+    public long Size { get; set; }
+}
+
+public class WhiteList
+{
+    public static readonly Dictionary<string, FileMetadata> AllowedFiles = new Dictionary<string, FileMetadata>
+    {
+        
+    };
+}
